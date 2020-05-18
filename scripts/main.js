5adfbfe42d347f451a74eb58ae72d290de5e4881
Simona <see.moona@yahoo.com>
2020-05-18 14:43:13 +0200

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ import { data1 } from "./products.js";
 let data = JSON.parse(data1);
 
 // //////////////VARIABLES///////////
+let logo = document.querySelector(".logo p");
 let pizza_options = document.querySelector(".menu_options");
 let orders = document.querySelector(".items-no");
 let payment = document.querySelector(".payment");
@@ -29,6 +30,9 @@ window.addEventListener("load", content);
 pizza.addEventListener("click", content);
 // ////////////MAIN FUNCTION/////////
 function content() {
+ // pizza.style.cssText='border-bottom:2px solid rgb(248, 110, 110);'
+  // salad.style.cssText='none';
+  // drink.style.cssText='none';
   pizza_options.innerHTML='';
   for (let i = 0; i < data.length; i++) {
     pizza_options.innerHTML += `<div class="item ">
@@ -62,9 +66,9 @@ function content() {
       <div class="pizza_name">${data[i]["name"]}</div>
       <div class="pizza_description">${data[i]["info"]}</div>
       <p class="pizza_size">Size:
-        <span class="choose">Small</span>
-        <span class="choose">Medium</span>
-        <span class="choose">Large</span>
+        <span class="sizes">Small</span>
+        <span class="sizes">Medium</span>
+        <span class="sizes">Large</span>
       </p>
       <p class="options"> Options:
         <span class="choose">Onions</span>
@@ -88,6 +92,7 @@ function content() {
 
       // ////////VARIABLES DECLARATION AFTER INSERTING PIZZA ITEMS////
       let selected = document.querySelectorAll(".choose");
+      let sizes = document.querySelectorAll(".sizes");
       let pay = document.querySelector(".pay");
       let go_pay = document.querySelector(".go_pay");
       let back_icon = document.querySelector(".fa-arrow-left");
@@ -161,15 +166,47 @@ function content() {
           2
         );
       }
+      // /////SIZE FOR THE PIZZA
+      sizes[0].style.backgroundColor = "rgb(248, 110, 110)";
+      const original=parseFloat(total.innerHTML);
+      let p=parseFloat(total.innerHTML);
+      sizes.forEach((element) => {
+        element.addEventListener("click", () => {
+          if(element.innerText=='Small'){
+            total.innerHTML=original
+            element.style.backgroundColor = "rgb(248, 110, 110)";
+            sizes[1].style.backgroundColor = "rgb(98, 91, 91)";
+            sizes[2].style.backgroundColor = "rgb(98, 91, 91)";
+          }
+          if(element.innerText=='Medium'){
+            total.innerHTML=(p+parseFloat(1)).toFixed(2);
+            element.style.backgroundColor = "rgb(248, 110, 110)";
+            sizes[0].style.backgroundColor = "rgb(98, 91, 91)";
+            sizes[2].style.backgroundColor = "rgb(98, 91, 91)";
+          }
+          if(element.innerText=='Large'){
+            total.innerHTML=(p+parseFloat(1.50)).toFixed(2);
+            element.style.backgroundColor = "rgb(248, 110, 110)";
+            sizes[0].style.backgroundColor = "rgb(98, 91, 91)";
+            sizes[1].style.backgroundColor = "rgb(98, 91, 91)";
+          }
+        })
+      })
+
+
       // ////SELECT EXTRAS FOR THE PIZZA
+      
       selected.forEach((element) => {
         element.addEventListener("click", () => {
+         
           if (element.style.backgroundColor == "rgb(248, 110, 110)") {
             element.style.backgroundColor = "rgb(98, 91, 91)";
-            total.innerHTML=parseFloat(total.innerHTML)-parseFloat(0.50);
+            total.innerHTML=(parseFloat(total.innerHTML)-parseFloat(0.50)).toFixed(2);
+            p=parseFloat(total.innerHTML)-0.50;
           } else {
             element.style.backgroundColor = "rgb(248, 110, 110)";
-            total.innerHTML=parseFloat(total.innerHTML)+parseFloat(0.50);
+            total.innerHTML=(parseFloat(total.innerHTML)+parseFloat(0.50)).toFixed(2);
+            p=parseFloat(total.innerHTML)-0.50;
           }
         });
       });
@@ -242,3 +279,8 @@ showAll.addEventListener("click", () => {
   }
   location.reload();
 });
+
+/////LOGO CLICK////
+logo.addEventListener('click',()=>{
+  location.reload();
+})
